bd48b004ba001620c986bacd07b3cca2fa691e6c
Mooseyyyy <hoekb24@gmail.com>
2024-02-01 17:07:45 -0700

    Updated project

--- a1/A1.py
+++ a1/A1.py
@@ -1,14 +1,29 @@
 #Imports
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
-import pandas as pd 
+import csv
 
-df = pd.read_csv("./news.csv")
+reader = csv.reader(open('news.csv'))
 
-text_tokens = []
-count = 0
-for text in df['text']:
-    words_tokens = word_tokenize(text)
-    text_tokens.append(words_tokens)
+tokens = []
+for line in reader:
+    for field in line:
+        tokens.append(word_tokenize(field))
 
-print(len(text_tokens))
+def unique(list1): 
+    unique_list = []
+    for x in list1:
+        if x not in unique_list:
+            unique_list.append(x)
+    print(len(unique_list))
+
+#Part A
+print("Part a.) With Stopwords")
+print("The number of word tokens in the corpus is: ", len(tokens))
+print("The number of unique words are: ", unique(tokens))
+
+#Part B
+stop_words = set(stopwords.words("english"))
+stop_tokens = tokens
+
+print("Part b.) Without Stopwords")
