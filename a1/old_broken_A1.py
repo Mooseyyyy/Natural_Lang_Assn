3eb3074cdf30595dc9a59f98238c775078b2a675
Mooseyyyy <hoekb24@gmail.com>
2024-02-04 22:31:12 -0700

    Update

--- a1/old_broken_A1.py
+++ a1/old_broken_A1.py
@@ -16,8 +16,7 @@ reader = csv.reader(open('news.csv'))
 tokens = []
 for line in reader:
     for field in line:
-        for x in field:
-            tokens.append(word_tokenize(x))
+        tokens.append(word_tokenize(field))
 
 # Function to pull out unique words
 def unique(list1): 
@@ -26,10 +25,50 @@ def unique(list1):
         if x not in unique_list:
             unique_list.append(x)
     print(len(unique_list))
-    print(tokens[:50])
 
-print("Tokens")
+#Part A
+print("Part a.) With Stopwords")
+print("The number of word tokens in the corpus is: ")
 print(len(tokens))
-print(tokens[:20])
-print("Unique Tokens")
+print("The number of unique words are: ")
 unique(tokens)
+
+# Part B
+stop_tokens = []
+stop_words = set(stopwords.words("english"))
+temp = tokens
+# Removing punctuation
+def remove_punctuation(not_removed):
+    trans = str.maketrans('', '', string.punctuation)
+    stripped = [x.translate(trans) for x in not_removed]
+    return stripped
+
+remove_punc = [remove_punctuation(x) for x in temp]
+
+for x in remove_punc:
+    for y in x:
+        if y not in stop_words:
+            stop_tokens.append(y)
+
+print("Part b.) Without Stopwords")
+print("The number of word tokens in the courpus is: ", len(stop_tokens))
+print("The number of unique words are: ", unique(stop_tokens))
+
+# Part C
+lem = WordNetLemmatizer()
+stoplem_tokens = []
+for x in stop_tokens:
+    stoplem_tokens.append(lem.lemmatize(x))
+
+print("Part c.) Without Stopwords and Lemmatization")
+print("The number of word tokens in the corpus is: ", len(stoplem_tokens))
+print("The number of unique words are: ", unique(stoplem_tokens))
+
+# Part D
+ps = PorterStemmer()
+stopstem_tokens = []
+for x in stop_tokens:
+    stopstem_tokens.append(ps.stem(x))
+print("Part d.) Without Stopwords and Stemming")
+print("The number of word tokens in the corpus is: ", len(stopstem_tokens))
+print("The number of unique words are: ", unique(stopstem_tokens))
