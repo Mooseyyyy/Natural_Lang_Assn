e6cfc8d1f08431eeb067f46f146a395744fa966c
Mooseyyyy <hoekb24@gmail.com>
2024-03-18 22:27:00 -0600

    A3 Update

--- a3/A3.py
+++ a3/A3.py
@@ -11,6 +11,8 @@ import numpy as np
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
 nltk.download("stopwords")
 
 df = pd.read_csv('news.csv')
@@ -25,6 +27,7 @@ stopwords_list = set(stopwords.words("english"))
 no_stopwords = ["not","don't",'aren','don','ain',"aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't",
                'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't",
                "won't", 'wouldn', "wouldn't"]
+
 for no_stopword in no_stopwords:
     stopwords_list.remove(no_stopword)
 
@@ -33,8 +36,8 @@ lemmatizer = WordNetLemmatizer()
 # function that receive a list of words and do lemmatization:
 def lemma_stem_text(words_list):
     # Lemmatizer
-    text = [lemmatizer.lemmatize(token.lower()) for token in words_list]# eighties->eight or messages->message or drugs->drug
-    text = [lemmatizer.lemmatize(token.lower(), "v") for token in text]# going-> go or started->start or watching->watch
+    text = [lemmatizer.lemmatize(token.lower()) for token in words_list]
+    text = [lemmatizer.lemmatize(token.lower(), "v") for token in text]
     return text
 
 import re
@@ -48,9 +51,51 @@ def negation_abbreviated_to_standard(sent):
 # We get the text of reviews in the training set
 reviews = x_train['review']
 
+def review_to_words(raw_review):
+    # 2. Transform abbreviated negations to the standard form.
+    review_text = negation_abbreviated_to_standard(review_text)
+
+    # 3. Remove non-letters and non-numbers
+    letters_numbers_only = re.sub("[^a-zA-Z_0-9]", " ", review_text)
+
+    # 4. Convert to lower case and split into individual words (tokenization)
+    words = np.char.lower(letters_numbers_only.split())
+
+    # 5. Remove stop words
+    meaningful_words = [w for w in words if not w in stopwords_list]
+
+    # 6. Apply lemmatization function
+    lemma_words = lemma_stem_text(meaningful_words)
+
+    # 7. Join the words back into one string separated by space, and return the result.
+    return( " ".join(lemma_words))
+
 # We initialize an empty list to add the clean reviews
 cleaned_train_reviews = []
 
-# We loop over each review and clean it  
-for i in reviews:
+# We loop over each review and clean it
+for i in X_train:
     cleaned_train_reviews.append(review_to_words(i))
+
+#Hyperparameters
+vectorizer = TfidfVectorizer(max_features = 20000, ngram_range = (1, 2))
+
+#Create training set with the words encoded as features of the reviews
+train_data_features = vectorizer.fit_transform(cleaned_train_reviews)
+
+#Define the model
+model = LogisticRegression(random_state = 0, solver = 'lbfgs' , multi_class = 'multinomial')
+
+#Train the model
+model.fit(train_data_features, y_train)
+
+num_reviews = len(X_test)
+clean_test_reviews = []
+
+for i in range(0, num_reviews):
+    clean_review = review_to_words(X_test[i])
+    clean_test_reviews.append(clean_review)
+
+test_data_features = vectorizer.transform(clean_test_reviews)
+
+result = model.predict(test_data_features)
